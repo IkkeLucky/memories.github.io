54552b55488dcc550da9d81ced0a80bb79e9186f
ikkelucky <nachomarchesini@gmail.com>
2024-01-18 14:40:08 +0100

    changes style and comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
+
+
 module.exports = {
     content: ["./node_modules/flowbite/**/*.js"],
     darkMode: 'class',
+    variants: {
+      extend: {
+        backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+        textColor: ['responsive', 'hover', 'focus', 'active'],
+      },
+    },
     theme: {
         extend: {
           colors: {
-            primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+            primary: {barbaraB: '#C2B280', "25": "#2da4aa", "50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
           }
         },
         fontFamily: {
